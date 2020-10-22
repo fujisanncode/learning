963e1c0037744da20e9ee7067376553e85523146
fujisanncode <812542982@qq.com>
2020-10-23 01:33:31 +0800

    sqlleaning迁移至learning

--- src/main/java/ink/fujisann/learning/code/controller/reader/ShelfController.java
+++ src/main/java/ink/fujisann/learning/code/controller/reader/ShelfController.java
@@ -1,11 +1,17 @@
 package ink.fujisann.learning.code.controller.reader;
 
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.github.xiaoymin.knife4j.annotations.ApiSort;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 /**
  * 书架
@@ -16,11 +22,29 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RestController
 @RequestMapping("/shelf")
+@Api(tags = "书架")
+@ApiSort(1)
 public class ShelfController {
 
+    /**
+     * 操作系统名称
+     */
+    public static final String OS_NAME = "os.name";
+
+    /**
+     * windows系统
+     */
+    public static final String WINDOWS = "windows";
+
+    @ApiOperationSupport(order = 1)
+    @ApiOperation("删除图书")
     @PostMapping("/uploadBook")
     public void uploadBook(MultipartFile file){
         String filename = file.getOriginalFilename();
+        if(System.getProperty("").toLowerCase().contains("windows")) {
+            new File("C:\\")
+            if()
+        }
         log.info(filename);
     }
 }
