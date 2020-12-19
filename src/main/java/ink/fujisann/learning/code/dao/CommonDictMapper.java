e44dcacbdfab93ee832173673c429453d4969696
fujisanCode <812542982@qq.com>
2020-12-19 23:26:56 +0800

    增加mybatis-plus，增加查询字典的mybatis拦截器

---
+++ src/main/java/ink/fujisann/learning/code/dao/CommonDictMapper.java
@@ -0,0 +1,9 @@
+package ink.fujisann.learning.code.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ink.fujisann.learning.code.pojo.mybatis.CommonDict;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommonDictMapper extends BaseMapper<CommonDict> {
+}
---
+++ src/main/java/ink/fujisann/learning/code/dao/CustomerMapper.java
@@ -0,0 +1,17 @@
+package ink.fujisann.learning.code.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ink.fujisann.learning.code.pojo.mybatis.Customer;
+import ink.fujisann.learning.code.resp.CustomerResp;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerMapper extends BaseMapper<Customer> {
+    /**
+     * 查询客户列表
+     * @return 客户列表
+     */
+    List<CustomerResp> listCustomer();
+}
